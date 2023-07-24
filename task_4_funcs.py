7dc590e4be5a9ad67d9169afeb4203c168e8417c
oldlama <48512419+oldlama@users.noreply.github.com>
2023-07-24 21:46:07 +0700

    Update task_4_funcs.py

--- task_4_funcs.py
+++ task_4_funcs.py
@@ -1,23 +1,23 @@
-def up_print(string):
+def up_print(string): #Правильно
 
     if string:
         print(string.upper())
 
 
-def letters_count_print(string):
+def letters_count_print(string): #Правильно
 
     if string:
         clear_string = ''
 
         for c in string:
             if c not in clear_string:
-                clear_string += c
+                clear_string += c  #Для подобного всё же лучше использовать лист, а не строку
 
         for c in clear_string:
             print(f'{c} - {string.count(c)}')
 
 
-def symbol_change_print(string):
+def symbol_change_print(string): #Всё нормально
 
     if string:
         old_symbol = input('Введите символ, который хотите заменить: ')
@@ -25,13 +25,14 @@ def symbol_change_print(string):
         print(string.replace(old_symbol, new_symbol))
 
 
-def words_from_string_print(string):
+def words_from_string_print(string): #Неправильно
 
     if string:
         box = ''
 
         for c in string:
-            if c.isalpha():
+            if c.isalpha():  ##Данный метод уберет так же цирфы, и остальные знаки. Это неправильно. НАпример при вводе
+                #емейла бот отработает совсем некорректно
                 box += c
             else:
                 box += ' '
