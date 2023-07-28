32eeb74f5299bd0e74547af6cd3fca749d75352b
meetMar <135348088+meetMar@users.noreply.github.com>
2023-07-28 19:47:29 +0700

    Add files via upload

--- task_5.py
+++ task_5.py
@@ -14,16 +14,11 @@
 # from random import randint
 #
 # nums_from_0_to_20 = [randint(0, 20) for _ in range(1000)]
-# nums_upward = sorted(set(nums_from_0_to_20))
-# nums_downward = sorted(set(nums_from_0_to_20), reverse=True)
-# box_1, box_2 = [], []
+# a, b = lambda x: x % 3 == 0, lambda x: x % 3 != 0
 #
-# for num in tuple(set(nums_from_0_to_20)):
-#     box_1.append(num) if num % 3 == 0 else box_2.append(num)  # как можно меньше строк :D
-#
-# print(*nums_upward)
-# print(*nums_downward)
-# print(*(box_1 + box_2))
+# print(set(sorted(nums_from_0_to_20)))
+# print(set(sorted(nums_from_0_to_20, reverse=True)))
+# print(list(set(filter(a, nums_from_0_to_20))) + list(set(filter(b, nums_from_0_to_20))))
 
 #5
 # from random import randint, choice
@@ -31,8 +26,6 @@
 # statuses = ('backlog', 'in_work', 'done')
 # l_of_ls = [list((randint(0, 1000), choice(statuses))) for _ in range(1000)]
 #
-# box = list(filter(lambda x: x[1] == 'backlog', l_of_ls))
-# box += list(filter(lambda x: x[1] == 'in_work', l_of_ls))
-# box += list(filter(lambda x: x[1] == 'done', l_of_ls))
-#
-# print(box)
+# print(list(filter(lambda x: x[1] == 'backlog', l_of_ls)), end=' ')
+# print(list(filter(lambda x: x[1] == 'in_work', l_of_ls)), end=' ')
+# print(list(filter(lambda x: x[1] == 'done', l_of_ls)), end=' ')
