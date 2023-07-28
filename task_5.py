4dedeb819f1ba19d3331339b0c3518d802289e1f
meetMar <135348088+meetMar@users.noreply.github.com>
2023-07-28 22:48:24 +0700

    Add files via upload

--- task_5.py
+++ task_5.py
@@ -20,11 +20,18 @@
 # print(sorted(set(nums_from_0_to_20), key=lambda x: x % 3))
 
 #5
-# from random import randint, choice
-#
-# statuses = ('backlog', 'in_work', 'done')
-# l_of_ls = [list((randint(0, 1000), choice(statuses))) for _ in range(1000)]
-#
+from random import randint, choice
+
+
+def sort_condition(x):
+    return x[1] == 'done', x[1] == 'in_work', x[1] == 'backlog'
+
+
+statuses = ('backlog', 'in_work', 'done')
+l_of_ls = [list((randint(0, 1000), choice(statuses))) for _ in range(1000)]
+
+print(sorted(l_of_ls, key=sort_condition))
+
 # print(list(filter(lambda x: x[1] == 'backlog', l_of_ls)), end=' ')
 # print(list(filter(lambda x: x[1] == 'in_work', l_of_ls)), end=' ')
 # print(list(filter(lambda x: x[1] == 'done', l_of_ls)), end=' ')
